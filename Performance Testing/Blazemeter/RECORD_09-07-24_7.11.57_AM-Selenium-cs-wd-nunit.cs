7bb60c4a8de35cdd24e8885127aeac88729bbbb7
tasnimkabiroishee <160079352+tasnimkabiroishee@users.noreply.github.com>
2024-09-09 01:04:21 +0600

    Add files via upload

---
+++ Performance Testing/Blazemeter/RECORD_09-07-24_7.11.57_AM-Selenium-cs-wd-nunit.cs
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestCase>
+<selenese>
+	<command>comment</command>
+	<target><![CDATA[]]></target>
+	<value><![CDATA[Label: Test]]></value>
+</selenese>
+<selenese>
+	<command>resizeWindow</command>
+	<target><![CDATA[]]></target>
+	<value><![CDATA[1920,945]]></value>
+</selenese>
+<selenese>
+	<command>open</command>
+	<target><![CDATA[https://blazedemo.com/]]></target>
+	<value><![CDATA[]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[name=fromPort]]></target>
+	<value><![CDATA[169,16]]></value>
+</selenese>
+<selenese>
+	<command>select</command>
+	<target><![CDATA[name=fromPort]]></target>
+	<value><![CDATA[label=Portland]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[name=toPort]]></target>
+	<value><![CDATA[150,13]]></value>
+</selenese>
+<selenese>
+	<command>select</command>
+	<target><![CDATA[name=toPort]]></target>
+	<value><![CDATA[label=New York]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@value = "Find Flights"]]]></target>
+	<value><![CDATA[61,19]]></value>
+</selenese>
+<selenese>
+	<command>comment</command>
+	<target><![CDATA[]]></target>
+	<value><![CDATA[Label: Choose a flight]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@value = "Choose This Flight"]]]></target>
+	<value><![CDATA[105,13]]></value>
+</selenese>
+<selenese>
+	<command>comment</command>
+	<target><![CDATA[]]></target>
+	<value><![CDATA[Label: Purchase a flight]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Name"])]]]></target>
+	<value><![CDATA[127,13]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Name"])]]]></target>
+	<value><![CDATA[abv]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Address"])]]]></target>
+	<value><![CDATA[85,22]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Address"])]]]></target>
+	<value><![CDATA[12 gg]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "City"])]]]></target>
+	<value><![CDATA[70,20]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "City"])]]]></target>
+	<value><![CDATA[ry]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "State"])]]]></target>
+	<value><![CDATA[56,13]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "State"])]]]></target>
+	<value><![CDATA[df]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Zip Code"])]]]></target>
+	<value><![CDATA[62,10]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Zip Code"])]]]></target>
+	<value><![CDATA[123]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[id=cardType]]></target>
+	<value><![CDATA[98,12]]></value>
+</selenese>
+<selenese>
+	<command>select</command>
+	<target><![CDATA[id=cardType]]></target>
+	<value><![CDATA[label=American Express]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Credit Card Number"])]]]></target>
+	<value><![CDATA[122,17]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Credit Card Number"])]]]></target>
+	<value><![CDATA[1232456]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Year"])]]]></target>
+	<value><![CDATA[30,11]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Year"])]]]></target>
+	<value><![CDATA[2015]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Name on Card"])]]]></target>
+	<value><![CDATA[88,12]]></value>
+</selenese>
+<selenese>
+	<command>type</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Name on Card"])]]]></target>
+	<value><![CDATA[nm mm]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@id=string(//label[text() = "Remember me"])]]]></target>
+	<value><![CDATA[2,8]]></value>
+</selenese>
+<selenese>
+	<command>click</command>
+	<target><![CDATA[//input[@value = "Purchase Flight"]]]></target>
+	<value><![CDATA[87,15]]></value>
+</selenese>
+</TestCase>
